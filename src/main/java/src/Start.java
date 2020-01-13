f7992d7bdfb59070c1adadcc8b02985c4fb95f89
Mirosław Malik <mmiro390@gmail.com>
2020-01-13 21:48:04 +0100

    write and read record from database

--- src/main/java/src/Start.java
+++ src/main/java/src/Start.java
@@ -10,8 +10,11 @@ public class Start {
     public static void main(String[] args) {
 
         CarDao carDao=new CarDao();
-        Car car = new Car("Fiat", "126p", LocalDate.of(1990,2,22));
-        carDao.add(car);
+//        Car car = new Car("Fiat", "126p", LocalDate.of(1990,2,22));
+//        carDao.add(car);
+
+        Car car = carDao.getCar(1L);
+        System.out.println(car);
     }
 
 }
