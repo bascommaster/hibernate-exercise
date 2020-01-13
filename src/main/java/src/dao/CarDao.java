3717293f74c8a2e2f362ea9cc45faf45e4b84dc1
Mirosław Malik <mmiro390@gmail.com>
2020-01-13 14:57:42 +0100

    add CarCao class with getCar and add methods

---
+++ src/main/java/src/dao/CarDao.java
@@ -0,0 +1,36 @@
+package src.dao;
+
+import org.hibernate.Session;
+import src.HibernateConfig;
+import src.entity.Car;
+
+
+
+public class CarDao {
+
+    private HibernateConfig hibernateConfig;
+
+
+    public CarDao() {
+        this.hibernateConfig = new HibernateConfig();
+    }
+
+
+    public Car getCar(Long id){
+
+        Session session = hibernateConfig.getSessionFactory().openSession();
+        Car car = session.get(Car.class, id);
+        session.close();
+
+        return car;
+    }
+
+
+    public void add(Car car){
+
+        Session session = hibernateConfig.getSessionFactory().openSession();
+        session.save(car);
+        session.close();
+
+    }
+}
