90d3b4220ddef1cc44d4b936b7825eeb8ed60adb
Mirosław Malik <mmiro390@gmail.com>
2020-01-13 15:13:20 +0100

    add Start class and create new car

---
+++ src/main/java/src/Start.java
@@ -0,0 +1,17 @@
+package src;
+
+import src.dao.CarDao;
+import src.entity.Car;
+
+import java.time.LocalDate;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        CarDao carDao=new CarDao();
+        Car car = new Car("Fiat", "126p", LocalDate.of(1990,2,22));
+        carDao.add(car);
+    }
+
+}
